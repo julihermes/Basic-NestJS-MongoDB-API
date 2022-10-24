52dcc463843581e1c1786866023a59478b00dfad
Julihermes Melo <julihermes@hotmail.com>
2022-10-24 13:30:21 -0300

    Small refactor in UserService

--- src/resources/users/users.service.ts
+++ src/resources/users/users.service.ts
@@ -8,46 +8,46 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  private UserRepository: EntityRepository<UserDocument>;
+  private userRepository: EntityRepository<UserDocument>;
 
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
-    this.UserRepository = new EntityRepository<UserDocument>(userModel);
+    this.userRepository = new EntityRepository<UserDocument>(userModel);
   }
 
   list() {
-    return this.UserRepository.find();
+    return this.userRepository.find();
   }
 
   show(id: string) {
-    return this.UserRepository.findById(id).then((user) => {
+    return this.userRepository.findById(id).then((user) => {
       if (!user) throw new NotFoundException(`User not found with id '${id}'`);
       return user;
     });
   }
 
   create(createUserDto: CreateUserDto) {
-    return this.UserRepository.create(createUserDto);
+    return this.userRepository.create(createUserDto);
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
-    return this.UserRepository.findByIdAndUpdate(id, updateUserDto).then(
-      (user) => {
+    return this.userRepository
+      .findByIdAndUpdate(id, updateUserDto)
+      .then((user) => {
         if (!user)
           throw new NotFoundException(`User not found with id '${id}'`);
         return user;
-      },
-    );
+      });
   }
 
   remove(id: string) {
-    return this.UserRepository.findByIdAndDelete(id).then((user) => {
+    return this.userRepository.findByIdAndDelete(id).then((user) => {
       if (!user) throw new NotFoundException(`User not found with id '${id}'`);
       return user;
     });
   }
 
   findByEmail(email: string) {
-    return this.UserRepository.findOne({ email }).then((user) => {
+    return this.userRepository.findOne({ email }).then((user) => {
       if (!user)
         throw new NotFoundException(`User not found with email '${email}'`);
       return user;
